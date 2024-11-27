e98a6921efadbd3221b9fe8b8cdabe010701c435
Chimary7 <jazmin.melo@usach.cl>
2024-11-27 10:50:42 -0300

    modifiacion a ejecutables

--- pdfFileService/src/main/java/com/example/pdfFileService/PdfFileServiceApplication.java
+++ pdfFileService/src/main/java/com/example/pdfFileService/PdfFileServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.pdfFileService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PdfFileServiceApplication {
 
 	public static void main(String[] args) {
