e0555f7fb274e3630cbe4b6e0eaa73827b38ce07
Chimary7 <jazmin.melo@usach.cl>
2024-12-02 15:37:21 -0300

    mod config

--- config-service/src/main/java/com/tutorial/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/tutorial/configservice/ConfigServiceApplication.java
@@ -3,6 +3,10 @@ package com.tutorial.configservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -12,4 +16,17 @@ public class ConfigServiceApplication {
 		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.addAllowedOrigin("");
+		corsConfig.addAllowedMethod("");
+		corsConfig.addAllowedHeader("*");
+		corsConfig.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
 }
