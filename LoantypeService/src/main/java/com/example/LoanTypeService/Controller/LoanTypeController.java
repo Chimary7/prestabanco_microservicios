4f29626d0cb3643188e85a41da3454027ff7e577
Chimary7 <jazmin.melo@usach.cl>
2024-12-02 20:41:46 -0300

    se modifica controlador loan y gateway

--- LoantypeService/src/main/java/com/example/LoanTypeService/Controller/LoanTypeController.java
+++ LoantypeService/src/main/java/com/example/LoanTypeService/Controller/LoanTypeController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/loanType")
-@CrossOrigin("*")
 public class LoanTypeController {
     @Autowired
     LoanTypeService loanTypeService;
