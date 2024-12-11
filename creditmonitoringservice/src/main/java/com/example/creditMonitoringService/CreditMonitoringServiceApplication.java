ce95be55d0de018bd8e72a93d0a6d2b7197da147
Chimary7 <jazmin.melo@usach.cl>
2024-12-11 18:34:42 -0300

    cambios en pom/ deploy y application en monitoring

--- creditmonitoringservice/src/main/java/com/example/creditMonitoringService/CreditMonitoringServiceApplication.java
+++ creditmonitoringservice/src/main/java/com/example/creditMonitoringService/CreditMonitoringServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.creditMonitoringService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CreditMonitoringServiceApplication {
 
 	public static void main(String[] args) {
